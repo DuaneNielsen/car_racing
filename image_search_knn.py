b60b982b7b601f3cccd3e3c5e7ce628cfa0f2800
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-10-31 11:57:40 -0700

    Image search.

---
+++ image_search_knn.py
@@ -0,0 +1,58 @@
+from lshash import lshash
+import numpy as np
+import keypoints as kps
+import geometry as geo
+from matplotlib import pyplot as plt
+
+i, query_set = 3, 2
+sdfs = np.load(f'data/ep{i}_sdf_road.npy')[70:]
+states = np.load(f'data/ep{i}_state.npy')[70:]
+sdfs_q = np.load(f'data/ep{query_set}_sdf_road.npy')[70:]
+states_q = np.load(f'data/ep{query_set}_state.npy')[70:]
+state = np.load(f'data/ep{i}_state.npy')
+
+N, h, w = sdfs.shape
+sample_i = geo.grid_sample(h, w, 12, pad=4)
+
+
+keypoints = []
+for i, sdf in enumerate(sdfs):
+    keypoints.append(kps.extract_kp(sdf, sample_i, iterations=3))
+keypoints = np.stack(keypoints)
+
+
+def knn(query, num_results=3):
+    distance = keypoints - query.reshape(1, *query.shape)
+    distance = distance ** 2
+    distance = distance.sum(axis=1)
+    distance = np.sqrt(distance)
+    distance = distance.mean(axis=1)
+    return np.argsort(distance)[0:num_results]
+
+
+fig = plt.figure()
+axes = fig.subplots(2, 4)
+query_plt = axes[0, 0]
+results_ax = axes[0, 1:4]
+query_state_plt = axes[1, 0]
+results_state_ax = axes[1, 1:4]
+
+sdf_q = sdfs[0]
+
+skip = 0
+for sdf_q, state_q in zip(sdfs_q, states_q):
+    skip += 1
+    if skip % 3 != 0:
+        continue
+    for row in axes:
+        for ax in row:
+            ax.clear()
+    query_plt.imshow(sdf_q)
+    query_state_plt.imshow(state_q)
+    kp_q = kps.extract_kp(sdf_q, sample_i, iterations=3)
+    top_k = knn(kp_q, num_results=3)
+    for i, result_ax, result_state_ax in zip(top_k, results_ax, results_state_ax):
+        result_ax.imshow(sdfs[i])
+        result_state_ax.imshow(states[i])
+    plt.pause(0.01)
+plt.show()
