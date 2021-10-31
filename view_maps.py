c9dc3c12117e4b769d0465d98406da9b5fbcc377
duanenielsen <duane.nielsen.rocks@gmail.com>
2021-10-31 11:56:19 -0700

    Add view maps.

---
+++ view_maps.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pathlib
+
+
+if __name__ == '__main__':
+    files = pathlib.Path('data').glob('*_map.npy')
+    maps = [np.load(f.absolute()) for f in files]
+    fig = plt.figure(figsize=(18, 18))
+    axes = fig.subplots(1)
+
+    for amap in maps:
+        axes.clear()
+        axes.imshow(amap[500:1500, 750:1750])
+        plt.pause(2.0)
+    plt.show()
+
